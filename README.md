d8511960d3abb9fee9ee5d4993927f3d5956f132
Devalon-Enter <faberlorin@hotmail.com>
2023-11-14 15:18:31 +0100

    Bad code added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # M226B
+
+## Analyse 
+Dieser Ansatz ist natürlich sehr mühsam und fehleranfällig
+Nennen Sie die Hauptprobleme bei diesem Ansatz:
+
+Starten wir als erstes mit der Klasse EventPost.
+Als
--- SocialNetwork/src/NewsFeed.java
+++ SocialNetwork/src/NewsFeed.java
@@ -1,3 +1,5 @@
+import jdk.jfr.Event;
+
 import java.util.ArrayList;
 
 public class NewsFeed {
@@ -5,12 +7,14 @@ public class NewsFeed {
 	
 	private ArrayList<MessagePost> messages;
 	private ArrayList<PhotoPost> photos;
+	private ArrayList<EventPost> events;
 	
 	
 	public NewsFeed() {
 		
 		messages = new ArrayList<>();
 		photos = new ArrayList<>();
+		events = new ArrayList<>();
 	}
 	
 	
@@ -20,6 +24,10 @@ public class NewsFeed {
 	
 	public void addPhotoPost(PhotoPost photo) {
 		photos.add(photo);
+	}
+
+	public void addEventPost(EventPost event) {
+		events.add(event);
 	}
 	
 	public void show() {
@@ -31,6 +39,11 @@ public class NewsFeed {
 		
 		for(PhotoPost photo : photos) {
 			photo.display();
+			System.out.println();
+		}
+
+		for(EventPost event : events) {
+			event.display();
 			System.out.println();
 		}
 		
